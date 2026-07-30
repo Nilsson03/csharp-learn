0a22802067e1d6ba7edccb92079f5b8df1129e66
Kuznecov N.A. <kuznecov.na@ocean.ru>
2026-07-30 11:24:42 +0300

    Командная детская игра, обучающая делению

---
+++ Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write(" fizzbuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write(" fizz");
+    } else if (i % 5 == 0)
+    {
+        Console.Write(" buzz");
+    } else
+    {
+        Console.Write($" {i}");
+    }
+}
